6f455d54581947508debc35bf9f4a493f453dcd0
Juan Carlos Pulido <jpulido@mdsdigital.com>
2022-11-02 18:17:19 -0500

    News section

--- table-news.js
+++ table-news.js
@@ -1,24 +1,47 @@
 (function(){
-
+  const ALL = 'All';
   const TITLE_ALL = 'SGV & Portfolio News';
-  const newsTitleFilter = document.getElementById('newsTitleFilter');
+  const newsTitleFilter = document.getElementById('newsTitleFilter').querySelector('h1');
   newsTitleFilter.innerText = TITLE_ALL;
 
-  const el = document.querySelector('#newsContent');
-  const rows = el.querySelector('table').querySelector('tbody').querySelectorAll('tr');
-  console.log(rows);
+  const rows = document.getElementById('tableNews').querySelector('tbody').querySelectorAll('tr');
 
   const companies = [];
+  const years = [];
   
+  // rows.forEach(row => {
+  //   const date = row.children[0].textContent;
+  //   const company = row.children[1].textContent;
+
+  //   if (!companies.includes(company)) {
+  //     companies.push(company);
+  //   }
+
+  //   row.dataset.company = company;
+  //   row.dataset.year = new Date(date).getFullYear();
+  // });
+
+  // Gel all companies from dataset company
   rows.forEach(row => {
-    const company = row.children[1].textContent;
+    const company = row.getAttribute('data-company');
+    // const year = row.getAttribute('data-year');
+
     if (!companies.includes(company)) {
       companies.push(company);
     }
+
+    // if (!years.includes(year)) {
+    //   years.push(year);
+    // }
+  });
+  companies.sort((a,b) => {
+    const lca = a.toLowerCase(), lcb = b.toLowerCase();
+    return lca > lcb ? 1 : lca < lcb ? -1 : 0;
   });
 
  const selectCompanies = document.getElementById('selectCompanies');
 
+ // Assign the options to select
  companies.map(company => {
   const option = document.createElement('option');
   option.setAttribute('value', company);
@@ -27,12 +50,20 @@
   selectCompanies.appendChild(option);
  });
 
+ // Listener when select option change
  selectCompanies.addEventListener('change', e => {
   const { value } = e.target;
-  newsTitleFilter.innerText = value === 'All' ? TITLE_ALL : value;
- });
+  if (!value) return;
+  newsTitleFilter.innerText = value === ALL ? TITLE_ALL : value;
 
-  // if table.dataset.type then get data from dataset.company in tr
-  // else get data from column company
+  rows.forEach(row => {
+    const company = row.getAttribute('data-company');
+    if (value !== ALL) {
+      row.style.display = company === value ? 'block' : 'none';
+      return;
+    }
+    row.style.display = 'block';
+  });
+ });
 
 })();
